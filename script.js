834e4be6bdc3034a4ed8fdd27f8b5df8b5ef6bfd
Franco Guerra <eugeniafilippin@hotmail.com>
2022-10-14 12:18:02 -0300

    :tada: Feat: carga de script vinculado con html

---
+++ script.js
@@ -0,0 +1,31 @@
+//String
+var nombre = "Franco";
+//String
+var apellido = "Guerra";
+//Number
+var edad = 28;
+//String
+const fechaDeNacimiento = "28/03/1994";
+//String
+var direccion = "Calle falsa 123";
+//String
+var colorFavorito = "Azul";
+//String
+var comidaFavorita = "Bife con papas fritas";
+//Boolean
+var soltero = false;
+//String
+var musicaFavorita = "Rock";
+//String
+var peliculaFavorito = "Toy Story 2";
+//Objeto
+var compañeros = { 
+    nombre: "Claudio",
+    apellido: "Gonzalez",
+    edad: 28,
+    fechaDeNacimiento: "28/03/1994",
+    //Array
+    mascotas:[{nombre:"Manchitas",tipo:"Perro"},{nombre:"Mish",tipo:"gato"}],
+    direccion: "Calle falsa 456"
+    };
+    
